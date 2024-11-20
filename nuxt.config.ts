aedb617ef67bdc710cf52f05deeb9bdaf930c86c
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-11-20 11:50:00 +0900

    render相關設定相關設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  app: {
+    head: {
+      title: "My Nuxt SSR App",
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 });
